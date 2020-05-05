d1b5c5a18a9b4c1574a34d0123a0211a7f8f6129
Drifty <pedro.lahuerta.blanco@gmail.com>
2020-05-05 12:15:17 +0200

    make it better

--- commands/ban.js
+++ commands/ban.js
@@ -1,14 +1,40 @@
 exports.run = (client, message, args, Discord) => {
-	// Tomar la primera mención del mensaje
-  let member = message.mentions.members.first(); // Array / arreglo
+  if(message.member.hasPermission("ADMINISTRATOR")){
+    // Notify the developers
+    console.log("[IMPORTANT]: " + message.author.tag + " ha baneado a alguien");
+    message.channel.send(message.author.tag +", baneando...");
+    try{
+    // Tomar la primera mención del mensaje
+    let member = message.mentions.members.first(); // Array / arreglo
+  
+    let memberUsername = member.tag; // Nombre#1234
+  
+    // Banear a la mención '@'
+    member.ban().then((member) => {
+      message.channel.send(message.author.tag + "Baneando...");
+    });  // Notificar al servidor de que el usuario ha sido baneado
+    
+  
+    }catch(err){
+      console.error(err);
+    }
+    // Notify developers and command issuer
+    console.log("[IMPORTANT]: Finalmente ha baneado correctamente");
+    message.reply("Has baneado correctamente");
+    try{
+      // Finish by deleting this command's cache
+      
+    }catch(err){
+      console.error(err);
+    }
+    // Notify developers
+    console.log("[IMPORTANT]: Reloads finished.");
+  } else {
+    message.reply("No tienes permisos para ejecutar ese comando.");
+    console.log("[INFO]: The command couldn't be executed because the user is not an administrator.");
+  }
 
-  let memberUsername = member.tag; // Nombre#1234
-
-	// Banear a la mención '@'
-  member.ban().then((member) => {
-    // Notificar al servidor de que el usuario ha sido baneado
-    message.channel.send('A sido baneado correctamente.');
-  });
+  
 };
 
 /*
--- commands/reload.js
+++ commands/reload.js
@@ -3,18 +3,20 @@ exports.run = (client, message, args, Discord) => {
     if(message.member.hasPermission("ADMINISTRATOR")){
       // Notify the developers
       console.log("[IMPORTANT]: " + message.author.tag + " está recargando los comandos.");
-      message.channel.send("¡Recargando!");
       try{
         // Delete cache
         delete require.cache[require.resolve('./server.js')];
         delete require.cache[require.resolve('./kick.js')];
+        delete require.cache[require.resolve('./dog.js')];
+        delete require.cache[require.resolve('./ban.js')];
+        delete require.cache[require.resolve('./unban.js')];
 
       }catch(err){
         console.error(err);
       }
       // Notify developers and command issuer
       console.log("[IMPORTANT]: Commands reloaded succesfully. Finishing reloads.");
-      message.reply("All commands reloaded.");
+      message.send("¡Recargado correctamente," + message.author.tag);
       try{
         // Finish by deleting this command's cache
         delete require.cache[require.resolve('./reload.js')];
