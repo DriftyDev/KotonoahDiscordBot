46cf8b618eb9dcd563addfacafe87d404d632e34
Drifty <pedro.lahuerta.blanco@gmail.com>
2020-05-10 21:57:46 +0200

    morememes

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ if(message.content.startsWith(prefix+"virgen")){
  
 
 if(message.content.startsWith(prefix+"meme")){
-  number = 20;
+  number = 61;
   var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
   switch (random) {
       case 1: message.channel.send("**Compra miembro**", {files: ["./images/1.jpg"]}); break;
@@ -115,6 +115,46 @@ if(message.content.startsWith(prefix+"meme")){
       case 20: message.channel.send("**hacker.exe**", {files: ["./images/meme14.jpg"]}); break;
       case 21: message.channel.send("**Spook.exe**", {files: ["./images/meme15.jpg"]}); break;
       case 22: message.channel.send("**Katto.exe**", {files: ["./images/meme16.jpg"]}); break;
+      case 23: message.channel.send("**Meme**", {files: ["./images/meme17.jpg"]}); break;
+      case 24: message.channel.send("**Meme**", {files: ["./images/meme18.jpg"]}); break;
+      case 25: message.channel.send("**Meme**", {files: ["./images/meme19.jpg"]}); break;
+      case 26: message.channel.send("**Meme**", {files: ["./images/meme20.jpg"]}); break;
+      case 27: message.channel.send("**Meme**", {files: ["./images/meme21.jpg"]}); break;
+      case 28: message.channel.send("**Meme**", {files: ["./images/meme22.jpg"]}); break;
+      case 29: message.channel.send("**Meme**", {files: ["./images/meme23.jpg"]}); break;
+      case 30: message.channel.send("**Meme**", {files: ["./images/meme24.jpg"]}); break;
+      case 31: message.channel.send("**Meme**", {files: ["./images/meme25.jpg"]}); break;
+      case 32: message.channel.send("**Meme**", {files: ["./images/meme26.jpg"]}); break;
+      case 33: message.channel.send("**Meme**", {files: ["./images/meme27.jpg"]}); break;
+      case 34: message.channel.send("**Meme**", {files: ["./images/meme28.jpg"]}); break;
+      case 35: message.channel.send("**Meme**", {files: ["./images/meme29.jpg"]}); break;
+      case 36: message.channel.send("**Meme**", {files: ["./images/meme30.jpg"]}); break;
+      case 37: message.channel.send("**Meme**", {files: ["./images/meme31.jpg"]}); break;
+      case 38: message.channel.send("**Meme**", {files: ["./images/meme32.jpg"]}); break;
+      case 39: message.channel.send("**Meme**", {files: ["./images/meme33.jpg"]}); break;
+      case 40: message.channel.send("**Meme**", {files: ["./images/meme34.jpg"]}); break;
+      case 41: message.channel.send("**Meme**", {files: ["./images/meme35.jpg"]}); break;
+      case 42: message.channel.send("**Meme**", {files: ["./images/meme36.jpg"]}); break;
+      case 43: message.channel.send("**Meme**", {files: ["./images/meme37.jpg"]}); break;
+      case 45: message.channel.send("**Meme**", {files: ["./images/meme38.jpg"]}); break;
+      case 46: message.channel.send("**Meme**", {files: ["./images/meme39.jpg"]}); break;
+      case 47: message.channel.send("**Meme**", {files: ["./images/meme40.jpg"]}); break;
+      case 48: message.channel.send("**Meme**", {files: ["./images/meme41.jpg"]}); break;
+      case 49: message.channel.send("**Meme**", {files: ["./images/meme42.jpg"]}); break;
+      case 50: message.channel.send("**Meme**", {files: ["./images/meme43.jpg"]}); break;
+      case 51: message.channel.send("**Meme**", {files: ["./images/meme44.jpg"]}); break;
+      case 52: message.channel.send("**Meme**", {files: ["./images/meme45.jpg"]}); break;
+      case 53: message.channel.send("**Meme**", {files: ["./images/meme46.jpg"]}); break;
+      case 54: message.channel.send("**Meme**", {files: ["./images/meme47.jpg"]}); break;
+      case 55: message.channel.send("**Meme**", {files: ["./images/meme48.jpg"]}); break;
+      case 56: message.channel.send("**Meme**", {files: ["./images/meme49.jpg"]}); break;
+      case 57: message.channel.send("**Meme**", {files: ["./images/meme50.jpg"]}); break;
+      case 58: message.channel.send("**Meme**", {files: ["./images/meme51.jpg"]}); break;
+      case 59: message.channel.send("**Meme**", {files: ["./images/meme52.jpg"]}); break;
+      case 60: message.channel.send("**Meme**", {files: ["./images/meme53.jpg"]}); break;
+      case 61: message.channel.send("**Meme**", {files: ["./images/meme54.jpg"]}); break;
+
+
   }
 
   }
